eab126f1f205f219afe80f01bfeb917a35bf2d73
lukas8971 <lukas.weber@os-solutions.at>
2016-01-23 12:24:41 +0100

    Some null-Test

--- ReplyBot/Test.cs
+++ ReplyBot/Test.cs
@@ -12,5 +12,51 @@ namespace MonoTests.ReplyBot
 			Assert.Throws<Exception>(()=>(new XMLHelper(null, null)));
 		}
 
+		[Test]
+		public void LoadTest(){
+			XMLHelper x = new XMLHelper ("test", "test");
+			x.Load ();
+			Assert.True ();
+		}
+
+
+		[Test]
+		public void SaveTest(){
+			XMLHelper x = new XMLHelper ("test", "test");
+			x.Save ();
+			Assert.True ();
+		}
+
 	}
+	[TestFixture]
+	public class TwitterHelperTest {
+
+		[Test]
+		public void SendTweetTestNull(){
+			Assert.Throws<Exception> (() => (TwitterHelper.SendTweet (null, null, null)));
+		}
+
+		[Test]
+		public void GetUserTimelineNull(){
+			Assert.Throws<Exception> (() => (TwitterHelper.GetUserTimeline (null, null, null, null)));
+		}
+
+		[Test]
+		public void GetUserIdFromUsernameNull(){
+			Assert.Throws<Exception> (() => (TwitterHelper.GetUserIdFromUsername (null, null)));
+		}
+
+
+	}
+	[TestFixture]
+	public class ReplyBotTest {
+
+		[Test]
+		public void MainTest (){
+			ReplyBot r = new ReplyBot ();
+			Assert.True;
+		}
+			
+	}
+	
 }
